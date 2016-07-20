7d4c82cf27de7f8cadd20d2161e48959a832b3ad
cavemandaveman <cavemandaveman@openmailbox.org>
2016-07-20 11:24:01 -0500

    Cleanup init script

--- 00-init-consul-template.sh
+++ 00-init-consul-template.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/bin/ln -s /var/run/s6/services/haproxy1 /haproxy-current
-/bin/ln -s /var/run/s6/services/haproxy2 /haproxy-alt
-/usr/local/bin/consul-template -config /consul-template/config.d -once
+ln -s /var/run/s6/services/haproxy1 /haproxy-current
+ln -s /var/run/s6/services/haproxy2 /haproxy-alt
+consul-template -config /consul-template/config.d -once
